197fefccba19c5b7cd0673395c409802edf03568
qiuyun <584233821@qq.com>
2014-12-30 11:20:49 +0800

    add snail project

---
+++ c_r/register/1.0.0/register.js
@@ -0,0 +1,13 @@
+/**
+ * Created with WebStorm.
+ * User: qiuyun
+ * Date: 14-12-29
+ * Time: 下午4:27
+ * To change this template use File | Settings | File Templates.
+ */
+var reg=function(){
+    reg.version="1.0.0";
+    reg.open=function(){
+        console.log('open reg');
+    }
+};
---
+++ publish/c/menu/1.0.0/menu.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>标题</title>
+    <meta name="description" content="描述"/>
+    <meta name="keywords" content="关键词"/>
+    <meta name="copyright" content="苏州蜗牛数字科技股份有限公司"/>
+    <meta name="author" content="senro"/>
+    <style>
+        .menu{}
+        .btn{}
+        .btn1{}
+        .btn2{}
+        .btn3{}
+    </style>
+</head>
+<body>
+<div class="menu">
+    <a class="btn btn1" href="#"></a>
+    <a class="btn btn2" href="#"></a>
+    <a class="btn btn3" href="#"></a>
+</div>
+<script type="text/javascript">
+    var menu=function(){};
+
+    menu();
+</script>
+</body>
+</html>
